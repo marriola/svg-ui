af1f8560da78efce11d512e6cb90148e6cde8d72
marriola <matt.arriola87@gmail.com>
2016-09-05 19:17:05 -0400

    Better centering

--- app/components/Titlebar.jsx
+++ app/components/Titlebar.jsx
@@ -28,7 +28,7 @@ let Titlebar = ({
             
             <text x={width / 2} y={height / 2}
                   clipPath={copyPathUri}
-                  textAnchor="middle"
+                  dominantBaseline="middle" textAnchor="middle"
                   onMouseDown={raiseWindow}>
                 {title}
             </text>
