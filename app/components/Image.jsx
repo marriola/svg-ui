702804d636553280489206cd37cee087d5c7214d
marriola <matt.arriola87@gmail.com>
2016-09-10 16:17:58 -0400

    Image allows scaling

--- app/components/Image.jsx
+++ app/components/Image.jsx
@@ -1,18 +1,53 @@
 import React from "react";
 
+/*
+ *  Displays an image
+ *
+ *  x
+ *  y
+ *  sw
+ *  sh
+ *  width
+ *  height
+ *  clipPath
+ */
 let Image = ({
     src,
     x = 0,
     y = 0,
+    sw, sh,
     width, height,
     clipPath,
-    onMouseUp,
-    onMouseDown,
-    onMouseLeave
-}) => (
-    <image xlinkHref={src} x={x} y={y} width={width} height={height} clipPath={clipPath}
-           onMouseUp={onMouseUp} onMouseDown={onMouseDown} onMouseLeave={onMouseLeave} />
-);
+    fit,
+    ...props
+}) => {
+    if (!sw) {
+        sw = width;
+        sh = height;
+    }
+    else if (!width) {
+        width = sw;
+        height = sh;
+    }
+    else if (fit && sw > width) {
+        let ratio = width / sw;
+        sw = width;
+        sh *= ratio;
+    }
+    else if (fit && sh > height) {
+        let ratio = height / sh;
+        sh = height;
+        sw *= ratio;
+    }
+
+    return (
+        <image {...props}
+               xlinkHref={src}
+               x={x + (width - sw) / 2} y={y + (height - sh) / 2}
+               width={sw} height={sh} clipPath={clipPath}
+        />
+    );
+};
 
 Image.propTypes = {
     x: React.PropTypes.number,
